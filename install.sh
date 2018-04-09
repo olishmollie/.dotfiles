ce6a2dd259216d0177a92cb3185ac5a805b61305
AJ <olishmollie@gmail.com>
2018-04-09 08:35:45 -0500

    Change dotfiles to .dotfiles

--- install.sh
+++ install.sh
@@ -6,14 +6,14 @@ set -e
 
 cp -R .vim $HOME
 
-cp $HOME/dotfiles/.bash_profile $HOME/.bash_profile
-cp $HOME/dotfiles/.vimrc $HOME/.vimrc
-cp $HOME/dotfiles/.spacemacs $HOME/.spacemacs
-cp $HOME/dotfiles/.inputrc $HOME/.inputrc
-cp $HOME/dotfiles/.irbrc $HOME/.irbrc
-cp $HOME/dotfiles/.gitignore_global $HOME/.gitignore_global
-cp $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-cp $HOME/dotfiles/.bashrc $HOME/.bashrc
+cp $HOME/.dotfiles/.bash_profile $HOME/.bash_profile
+cp $HOME/.dotfiles/.vimrc $HOME/.vimrc
+cp $HOME/.dotfiles/.spacemacs $HOME/.spacemacs
+cp $HOME/.dotfiles/.inputrc $HOME/.inputrc
+cp $HOME/.dotfiles/.irbrc $HOME/.irbrc
+cp $HOME/.dotfiles/.gitignore_global $HOME/.gitignore_global
+cp $HOME/.dotfiles/.gitconfig $HOME/.gitconfig
+cp $HOME/.dotfiles/.bashrc $HOME/.bashrc
 
 # Sets global gitignore
 git config --global core.excludesfile $HOME/.gitignore_global
@@ -24,7 +24,7 @@ cp olishmollie.uninstall.sh ~
 vim +PlugInstall +qall
 
 echo 'Installation complete.'
-echo 'Remember to set git user/email with `git config --global`'
+echo 'Remember to set git user.name and user.email with `git config --global`'
 echo 'Restart terminal to see changes.'
 echo 'To uninstall, run olishmollie.uninstall.sh'
 echo ''
