fb0871a6f3694f9d4734133e87a8e35548eec31b
ajbond <olishmollie@gmail.com>
2019-07-16 18:04:59 -0500

    add some built-in config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,3 +45,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;; Built-in config
+(electric-pair-mode t)
+(add-hook 'c-mode-common-hook #'c-mode-init)
