6aaa2e5ab828dc7c3013dd6455b6796b3b499000
ajbond <olishmollie@gmail.com>
2019-07-16 18:06:12 -0500

    leverage use-package a little more

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,36 +3,49 @@
 			 ("melpa-stable" . "https://stable.melpa.org/packages/")
 			 ("gnu" . "http://elpa.gnu.org/packages/")))
 
+(defun c-mode-init ()
+  "Initializes cc-mode."
+  (setq c-default-style "k&r")
+  (setq c-basic-offset 4))
+
+(defun go-mode-init ()
+  "Initializes go-mode."
+  (setq tab-width 4)
+  (setq gofmt-command "goimports")
+  (add-hook 'before-save-hook #'gofmt-before-save))
+
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-;; Install any missing packages.
-(let ((refreshed? nil))
-  (dolist (p package-selected-packages)
-    (unless (package-installed-p p)
-	(when (null refreshed?)
-	  (package-refresh-contents))
-	(package-install p))))
-
-;; Init packages.
 (eval-when-compile
-  (require 'use-package))
+  (unless (package-installed-p 'use-package)
+    (package-refresh-contents)
+    (install-package 'use-package))
+  (require 'use-package)
+  (setq use-package-always-ensure t))
 
 (use-package shell-pop)
-(use-package magit)
-(use-package go-mode)
+(use-package magit
+  :init (global-set-key (kbd "C-x g") 'magit-status))
+(use-package zenburn-theme
+  :init (load-theme 'zenburn t))
 (use-package flycheck
   :init (global-flycheck-mode))
+(use-package go-mode
+  :init
+  (add-hook 'go-mode-hook #'go-mode-init))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (flycheck go-mode use-package shell-pop magit)))
+ '(package-selected-packages
+   (quote
+    (zenburn-theme flycheck go-mode use-package shell-pop magit)))
  '(shell-pop-shell-type
    (quote
     ("ansi-term" "*ansi-term*"
