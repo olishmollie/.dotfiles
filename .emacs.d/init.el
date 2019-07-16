534efe6a00f4a6e688e3b2cae65878fcd3aae518
ajbond <olishmollie@gmail.com>
2019-07-16 13:30:08 -0500

    no evil

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,15 +26,13 @@
 (use-package go-mode)
 (use-package flycheck
   :init (global-flycheck-mode))
-(use-package evil
-  :init (evil-mode 1))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (go-mode use-package shell-pop evil magit)))
+ '(package-selected-packages (quote (flycheck go-mode use-package shell-pop magit)))
  '(shell-pop-shell-type
    (quote
     ("ansi-term" "*ansi-term*"
