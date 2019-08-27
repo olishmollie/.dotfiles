3f344439b0d9005427f04a06550793a31c0f9ec0
ajbond <olishmollie@gmail.com>
2019-08-27 15:12:33 -0500

    Modify emacs init file

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,8 +3,15 @@
 			 ("melpa-stable" . "https://stable.melpa.org/packages/")
 			 ("gnu" . "http://elpa.gnu.org/packages/")))
 
+(defun asm-mode-init ()
+  "Initializes asm-mode."
+    (setq tab-width 4)
+    (setq indent-line-function 'insert-tab)
+    (setq asm-indent-level 4))
+
 (defun c-mode-init ()
   "Initializes cc-mode."
+  (local-set-key (kbd "C-c C-f") 'clang-format-buffer)
   (setq c-default-style "k&r")
   (setq c-basic-offset 4))
 
@@ -16,8 +23,11 @@
 
 ;; Built-in config
 (electric-pair-mode t)
+(setq make-backup-files nil)
 (add-hook 'c-mode-common-hook #'c-mode-init)
+(add-hook 'asm-mode-hook #'asm-mode-init)
 (global-display-line-numbers-mode t)
+(global-auto-revert-mode t)
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
@@ -32,10 +42,9 @@
   (require 'use-package)
   (setq use-package-always-ensure t))
 
-(use-package projectile
-  :init (projectile-mode 1))
 (use-package smart-tab
-  :init (global-smart-tab-mode t))
+  :init
+  (global-smart-tab-mode t))
 (use-package shell-pop)
 (use-package magit
   :init (global-set-key (kbd "C-x g") 'magit-status))
@@ -46,7 +55,8 @@
   (global-flycheck-mode)
   (setq flycheck-python-pycompile-executable "python3"))
 (use-package go-mode
-  :init (add-hook 'go-mode-hook #'go-mode-init))
+  :init
+  (add-hook 'go-mode-hook #'go-mode-init))
 (use-package evil
   :init
   (evil-mode t)
@@ -63,7 +73,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (projectile smart-tab evil-magit evil-commentary zenburn-theme flycheck go-mode use-package shell-pop magit)))
+    (clang-format projectile smart-tab evil evil-magit evil-commentary zenburn-theme flycheck go-mode use-package shell-pop magit)))
  '(shell-pop-shell-type
    (quote
     ("ansi-term" "*ansi-term*"
