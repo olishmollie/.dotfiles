f44bb9b42b14e08e7df73247efc99e57b9a1274c
Andrew Bond <abond@pmeasuring.com>
2022-12-25 17:11:44 -0600

    Update mac install script

--- macos.uninstall.sh
+++ macos.uninstall.sh
@@ -5,20 +5,14 @@ read -r REPLY
 if [ "$REPLY" = 'y' ]
 then
     echo "Removing dotfiles..."
-    rm "$HOME/.zhrc"
+    rm "$HOME/.bash_profile"
+    rm "$HOME/.bashrc"
     rm "$HOME/.gitconfig"
     rm "$HOME/.gitignore_global"
     rm "$HOME/.inputrc"
 
     rm -rf "$HOME/.dotfiles"
-    rm -rf "$HOME/.oh-my-zsh"
-
-    echo "Uninstalling oh-my-zsh..."
-    uninstall_oh_my_zsh
 
     echo "Uninstalling Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
-
-    echo "Uninstalling Rust..."
-    yes | rustup self uninstall
+    /bin/bash -c "sudo $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall.sh)"
 fi
